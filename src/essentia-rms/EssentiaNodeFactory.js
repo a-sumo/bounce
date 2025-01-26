93b31f8d81c98198a1821fbb4883d142d628c2b8
a-sumo <armandsumo@gmail.com>
2025-01-26 13:54:14 +0100

    change essentia worklet path for remote

--- src/essentia-rms/EssentiaNodeFactory.js
+++ src/essentia-rms/EssentiaNodeFactory.js
@@ -11,8 +11,7 @@ export async function createEssentiaNode(context) {
   }
 
   try {
-    // Use ?worker&url for proper TypeScript/JavaScript processing
-    const workletPath = '/src/essentia-rms/EssentiaWorkletProcessor.js';
+    const workletPath = new URL('../essentia-rms/EssentiaWorkletProcessor.js', import.meta.url).href;
     await context.audioWorklet.addModule(workletPath);
   } catch(e) {
     console.error('Error loading audio worklet:', e);
